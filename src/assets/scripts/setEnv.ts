e8ed182c1986f35bd2126a16afb361a9858fa8b0
Artsiom Tratsiuk <inqui05@gmail.com>
2021-12-28 13:16:09 +0300

    feat: add eslint, prettier and vsc settings

---
+++ src/assets/scripts/setEnv.ts
@@ -0,0 +1,31 @@
+/*tslint: disable*/
+//@ts-nocheck
+
+const { writeFile, existsSync, mkdirSync } = require('fs');
+const { argv } = require('yargs');
+
+require('dotenv').config();
+const environment = argv.environment;
+
+function writeFileUsingFS(targetPath, environmentFileContent) {
+  writeFile(targetPath, environmentFileContent, function (err) {
+    if (err) console.log(err);
+    if (environmentFileContent !== '') console.log(`wrote variables to ${targetPath}`);
+  });
+}
+
+const envDirectory = './src/environments';
+if (!existsSync(envDirectory)) mkdirSync(envDirectory);
+
+writeFileUsingFS('./src/environments/environment.prod.ts', '');
+writeFileUsingFS('./src/environments/environment.ts', '');
+
+const isProduction = environment === 'prod';
+const targetPath = isProduction ? './src/environments/environment.prod.ts' : './src/environments/environment.ts';
+const environmentFileContent = `export const environment = {
+    production: ${isProduction},
+    moviesDatabaseKey: '${process.env.moviesDatabaseKey}',
+  }`;
+
+writeFileUsingFS(targetPath, environmentFileContent);
+/*tslint: enable*/

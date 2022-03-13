b54d958f3483ca8b9ddd7d53372c9e5a6d999e11
Artsiom Tratsiuk <inqui05@gmail.com>
2022-03-13 16:55:38 +0300

    feat: add movie page components to app

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -6,10 +6,14 @@ import CardComponent from './components/card/card.component';
 import CategorySwitcherComponent from './components/category-switcher/category-switcher.component';
 import PaginationComponent from './components/pagination/pagination.component';
 import MoviesComponent from './pages/movies/movies.component';
+import MoviePageComponent from './pages/movie-page/movie-page.component';
+import CoverComponent from './components/cover/cover.component';
+import PhotoComponent from './components/photo/photo.component';
 
 @NgModule({
-  declarations: [MoviesComponent, CategorySwitcherComponent, CardComponent, PaginationComponent],
+  declarations: [MoviesComponent, CategorySwitcherComponent, CardComponent, PaginationComponent,
+    MoviePageComponent, CoverComponent, PhotoComponent],
   imports: [CommonModule, SharedModule],
-  exports: [MoviesComponent],
+  exports: [MoviesComponent, MoviePageComponent],
 })
 export default class MoviesModule {}

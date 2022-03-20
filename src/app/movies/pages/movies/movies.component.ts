09608fabad7d330aa6a4a9a4fafc619817e2ea44
Artsiom Tratsiuk <inqui05@gmail.com>
2022-03-20 19:16:19 +0300

    feat: add responsive layout of app

--- src/app/movies/pages/movies/movies.component.ts
+++ src/app/movies/pages/movies/movies.component.ts
@@ -7,5 +7,16 @@ import { IPagination } from '../../models/pagination.model';
   styleUrls: ['./movies.component.scss'],
 })
 export default class MoviesComponent {
-  setPagination: IPagination = { itemsCount: 150, pageSize: 15 };
+  public CARDS_ON_PAGE_BY_DAFAULT = 20;
+
+  // TODO: remove magic numbers
+  setPagination: IPagination = { itemsCount: 150, pageSize: this.CARDS_ON_PAGE_BY_DAFAULT };
+
+  constructor() {
+    const { width } = window.screen;
+
+    if (width >= 822 && width <= 1239) {
+      this.CARDS_ON_PAGE_BY_DAFAULT = 21;
+    }
+  }
 }

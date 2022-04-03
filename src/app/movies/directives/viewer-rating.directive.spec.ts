582850acb34f47fea2d2d8e4544d547ba6e6584e
Artsiom Tratsiuk <inqui05@gmail.com>
2022-04-03 20:38:17 +0300

    feat: implement directive that change color of viewer reting

---
+++ src/app/movies/directives/viewer-rating.directive.spec.ts
@@ -0,0 +1,8 @@
+import ViewerRatingDirective from './viewer-rating.directive';
+
+describe('ViewerRatingDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ViewerRatingDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/movies/directives/viewer-rating.directive.ts
@@ -0,0 +1,31 @@
+import {
+  Directive, HostBinding, Input, OnInit,
+} from '@angular/core';
+
+export enum AvailableColor {
+  Green = '#3c3',
+  Orange = '#ed760e',
+  Gray = 'gray',
+  Red = 'red',
+}
+
+@Directive({
+  selector: '[appViewerRating]',
+})
+export default class ViewerRatingDirective implements OnInit {
+  @Input('appViewerRating') rating = 0;
+
+  @HostBinding('style.backgroundColor') bottomBorderColor = AvailableColor.Red;
+
+  ngOnInit(): void {
+    if (+this.rating >= 7) {
+      this.bottomBorderColor = AvailableColor.Green;
+    } else if (+this.rating >= 6) {
+      this.bottomBorderColor = AvailableColor.Orange;
+    } else if (+this.rating >= 4.5) {
+      this.bottomBorderColor = AvailableColor.Gray;
+    } else if (+this.rating < 4.5) {
+      this.bottomBorderColor = AvailableColor.Red;
+    }
+  }
+}
